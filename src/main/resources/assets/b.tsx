ed0d9df9428934fbe1f7653734be440755e0b009
Mohamedbd99 <mohamed.bendaamarr@gmail.com>
2025-12-24 19:57:30 +0100

    remove recourses from git ignore

---
+++ src/main/resources/assets/b.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2"
+         name="b"
+         tilewidth="64"
+         tileheight="64"
+         tilecount="1462"
+         columns="43">
+    <!-- Just the file name, since PNG is in the same folder as b.tsx -->
+    <image source="apocalypse tiles TRANSPARENT v1.1.0.png" width="2800" height="2176"/>
+</tileset>
